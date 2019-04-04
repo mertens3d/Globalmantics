ef1935328f5233f513ac12b8885a71640277b005
Gregory Mertens <gregory.mertens@Perficient.com>
2019-04-04 12:29:17 -0500

    Module 3 complete

---
+++ Globomantics/gulpfile.js
@@ -0,0 +1,27 @@
+﻿/// <binding Clean='clean' />
+"use strict";
+
+var gulp = require("gulp"),
+    concat = require("gulp-concat"),
+    cssmin = require("gulp-cssmin"),
+    uglify = require("gulp-uglify");
+
+gulp.task("min:js", function () {
+    //return gulp.src([paths.jqueryJs, paths.raphaelJs, paths.morrisJs])
+    //    .pipe(concat(paths.jsDest + "min.site.min.js"))
+    //    .pipe(ugligy())
+    //    .pipe(gulp.dest("."));
+    return null;
+});
+
+gulp.task("copy:js", function() {
+    return gulp.src([paths.jqueryJs, paths.raphaelJs, paths.morrisJs])
+        .pipe(gulp.dest(paths.jsDest));
+});
+
+gulp.task("min:css", function() {
+    return gulp.src([paths.BootstrapCss, paths.sbAdminCss, paths.fontAwesomeCss, paths.morrisJs])
+        .pipe(concat(paths.cssDest + "/min/site.min.css"))
+        .pipe(cssmin())
+        .pipe(gulp.dest("."));
+});
